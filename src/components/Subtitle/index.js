0aec421eee8cf38fa8f0133b63618281a665108b
leafartech <119262053+leafartech@users.noreply.github.com>
2023-05-26 08:28:10 -0300

    Delete src directory

--- src/components/Subtitle/index.js
+++
@@ -1,9 +0,0 @@
-import styles from './Subtitle.module.css'
-
-export default function Subtitle({ children, left, size, bg }) {
-    return (
-        <div className={ `${bg ? styles.bg : ''} ${size ? styles.size : ''} ${left ? styles.left : ''} ${styles.subtitle}` }>
-            <p>{children}</p>
-        </div>
-    )
-}
