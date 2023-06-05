4ceb1a864fa5824130feeba1dd2c9d71a85ee410
leafartech <119262053+leafartech@users.noreply.github.com>
2023-06-05 20:37:29 -0300

    Add files via upload

---
+++ src/components/Footer/index.js
@@ -0,0 +1,14 @@
+import styles from './Footer.module.css'
+
+export default function Footer({ secondary }) {
+    return (
+        <footer className={ styles.footer }>
+            {secondary ?
+            <img src="../images/logo.png" alt="Escritório KRÜGER TOLEDO advocacia" />
+            : 
+            <img src="./images/logo.png" alt="Escritório KRÜGER TOLEDO advocacia" />
+            }
+            <p>2023 KRÜGER TOLEDO © Todos os direitos reservados.</p>
+        </footer>
+    )
+}
