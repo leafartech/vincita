8529f32417e4884df6b779592810312c76fcd16d
leafartech <119262053+leafartech@users.noreply.github.com>
2023-06-05 16:54:53 -0300

    Delete src directory

--- src/components/pv/Template/index.js
+++
@@ -1,9 +0,0 @@
-import styles from './Template.module.css'
-
-export default function Template({children}) {
-    return (
-        <div className={ styles.template }>
-            {children}
-        </div>
-    )
-}
