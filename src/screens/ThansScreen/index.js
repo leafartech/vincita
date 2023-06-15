437e9ba938e5955351d8c2b583b7120f777e80e8
leafartech <119262053+leafartech@users.noreply.github.com>
2023-06-15 08:40:35 -0300

    Add files via upload

--- src/screens/ThansScreen/index.js
+++ src/screens/ThansScreen/index.js
@@ -1,15 +1,14 @@
 import Title from '@/components/Title'
 import styles from './ThanksScreen.module.css'
-import Subtitle from '@/components/Subtitle'
 
 export default function ThanksScreen() {
     return (
         <div className={ styles['thanks-screen']}>
             <div>
                 <Title>Muito obrigado por dar o primeiro passo</Title>
-                <Subtitle center={true}>Entraremos em contato o mais rápido possível via Whatsapp</Subtitle>
+                <p>Entraremos em contato o mais rápido possível via Whatsapp</p>
             </div>
-            <img src="./images/logo.png" alt="Logo do escritório KRÜGER TOLEDO" />
+            <img src="./imagesPv/logo.png" alt="Logo do escritório KRÜGER TOLEDO" />
         </div>
     )
 }
