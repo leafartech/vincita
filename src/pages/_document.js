1e2f0009bd0fa843573b60716f51c8a36c31e388
leafartech <119262053+leafartech@users.noreply.github.com>
2023-09-27 16:45:17 -0300

    Update _document.js

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -10,6 +10,17 @@ export default function Document() {
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin='true' />
         <link href="https://fonts.googleapis.com/css2?family=Source+Serif+4:wght@400;600;800&display=swap" rel="stylesheet" />
+         <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(c,l,a,r,i,t,y){
+                 c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                 t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                 y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+               })(window, document, "clarity", "script", "j1a2usrg69");
+            `,
+          }}
+        />
         {/* Google Tag Manager */}
         <script
           dangerouslySetInnerHTML={{
