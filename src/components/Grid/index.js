93659803101717dab28d1016694659b3cf8189a6
leafartech <119262053+leafartech@users.noreply.github.com>
2023-11-09 12:29:03 -0300

    Delete src directory

--- src/components/Grid/index.js
+++
@@ -1,9 +0,0 @@
-import styles from './Grid.module.css'
-
-export default function Grid({ children }) {
-    return (
-        <div className={ styles.grid }>
-            {children}
-        </div>
-    )
-}
--- src/pages/repactuacao-b/index.js
+++
@@ -1,8 +0,0 @@
-import HomeScreen2 from "@/screens/HomeScreen2";
-
-
-export default function Home() {
-    return (
-        <HomeScreen2 />
-    )
-}
