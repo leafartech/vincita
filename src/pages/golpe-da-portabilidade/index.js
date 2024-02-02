c336fc6ff514e194564322afc073b328c48cd81d
leafartech <119262053+leafartech@users.noreply.github.com>
2024-02-02 10:50:39 -0300

    Add files via upload

---
+++ src/pages/golpe-da-portabilidade/index.js
@@ -0,0 +1,9 @@
+import PortabilidadeScreen from "@/screens/PortabiliddeScreen";
+
+export default function Portabilidade() {
+    return (
+        <>
+            <PortabilidadeScreen />
+        </>
+    )
+}
